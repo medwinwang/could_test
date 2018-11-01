7ca147da1b05d09e64e060551c7f5193347d7f7c
medwin <wm1990315@gmail.com>
2018-11-01 13:07:56 +0800

    测试配置中心

--- eureka-producer/src/main/java/com/example/demo/controller/UserController.java
+++ eureka-producer/src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,21 +17,22 @@ import java.util.List;
 /**
  * Created by medwin on 2018/10/8.
  */
-
+@RefreshScope
 @RestController
+@Slf4j
 public class UserController {
 //
     @Autowired
     private DiscoveryClient client;
 
+
+    @Value("${host.info}")
+    private String info;
+
     @GetMapping("/hello/{name}")
     public String hello(@PathVariable String name){
 
-//        ServiceInstance instance=client.getInstances();
-//        logger.info("serviceId"+instance.getServiceId()+"host:post="+instance.getHost()+":"+instance.getPort());
-
-        System.out.println(name+" welcome . My is microservice provider user");
-        return name+" welcome . My is microservice provider user";
+        return name+" welcome . My is microservice provider user：" + info;
     }
 
     @RequestMapping("/service-instances/{applicationName}")
